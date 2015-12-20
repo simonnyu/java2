4b47ce8d5660c11243e18102b6bb6557f8937f89
Simon <Simon@Simon-PC>
2015-12-20 14:30:23 +0800

    1.0

---
+++ MyForm/src/form/codejava/MyForm.java
@@ -0,0 +1,53 @@
+package form.codejava;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyForm
+ */
+@WebServlet("/MyForm")
+public class MyForm extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyForm() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		String mail = request.getParameter("mail");
+		String birday = request.getParameter("birthday");
+		String address = request.getParameter("address");
+		out.println("<html>");
+		out.println("Name:"+name+"<br>");
+		out.println("E-Mail:"+mail+"<br>");
+		out.println("Birthday:"+birday+"<br>");
+		out.println("Address:"+address+"<br>");
+		out.println("</html>");
+		
+	}
+
+}
